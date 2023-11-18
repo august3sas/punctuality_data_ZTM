665bf2f1b04b7dac1ff04cd1663e53e9befd4e35
Jakub Brambor <111154360+august3sas@users.noreply.github.com>
2023-11-18 21:20:03 +0100

    new code

--- prepareformodel.py
+++ prepareformodel.py
@@ -2,7 +2,10 @@ import pandas as pd
 import numpy as np
 import urllib.request
 import geojson
-
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
 def replace_polish_chars(text):
     replacements = {'ą': 'a', 'ć': 'c', 'ę': 'e', 'ł': 'l', 'ń': 'n', 'ó': 'o', 'ś': 's', 'ź': 'z', 'ż': 'z'}
     for orig_char, repl_char in replacements.items():
@@ -11,7 +14,7 @@ def replace_polish_chars(text):
     return text
 
 df=pd.read_csv('dane_parsed.csv')
-print(df)
+#print(df)
 #nu     liczba_porzadkowa
 #Qua       rodzaj_pojazdu
 #nu       nr_boczny
@@ -28,10 +31,10 @@ print(df)
 #Qnt       godzina_f
 #nu       godzina_datetime
 qualitative=['rodzaj_pojazdu','linia','dzien_tygodnia','model']
-quantitative=['godzina_f','nastepny_przystanek']
+quantitative=['opoznienie_f','godzina_f','nastepny_przystanek']
 
 dataset=df[quantitative+qualitative]
-print(dataset)
+#print(dataset)
 
 target_url='https://www.poznan.pl/mim/plan/map_service.html?mtype=pub_transport&co=cluster'
 ztmdata=urllib.request.urlopen(target_url)
@@ -51,7 +54,7 @@ for feature in geodata_features:
         coords_name.append({'coords':coords, 'stop_name': name})
         unique_stops.add(name)
         i+=1
-#print(geodata_features[:,1])
+print(geodata_features)
 df_coords_name=pd.DataFrame(coords_name)
 #print(df_coords_name)
 merged_dataset=pd.merge(dataset,df_coords_name,left_on='nastepny_przystanek',right_on='stop_name',how='left')
@@ -62,3 +65,32 @@ merged_dataset.drop(columns=['coords'], inplace=True)
 merged_dataset.drop(columns=['nastepny_przystanek'], inplace=True)
 merged_dataset.drop(columns=['stop_name'], inplace=True)
 print(merged_dataset)
+
+fig,ax=plt.subplots()
+ax.scatter(merged_dataset['latitude'],merged_dataset['longtitude'],c=merged_dataset['opoznienie_f'])
+plt.show()
+
+
+
+feature_columns=['godzina_f','latitude','longtitude']
+test_columns=[
+    ['20.16666','16.95561834','52.41024294'],#3:30
+    ['20.25','16.92826965','52.39116115'],#1:05
+    ['20.25','16.92244559','52.39410497'],#0:12
+    ['20.25','16.92005776','52.43575246'],#0:13
+]
+y=merged_dataset['opoznienie_f']
+X=merged_dataset[feature_columns]
+train_X, val_X, train_y, val_y = train_test_split(X,y)
+for i in [5, 10, 50, 100]:
+    model_simple=DecisionTreeRegressor(max_leaf_nodes=i)
+    model_simple.fit(X,y)
+    val_predictions=model_simple.predict(val_X)
+
+    print(val_y.head())
+    print(val_predictions[:5])
+    print('leaf nodes = ', i,'mae = ',mean_absolute_error(val_y, val_predictions))
+    print('\n')
+
+val_predictions=model_simple.predict(test_columns)
+print(val_predictions)
